94eaa36810484d28b58b72f01dc6397a0368e239
bilejum <17762591607@163.com>
2023-02-18 19:57:42 +0800

    一个文件的判断方法

--- learnCode/src/Demo1/Demo01.java
+++ learnCode/src/Demo1/Demo01.java
@@ -1,4 +1,26 @@
 package Demo1;
 
+import java.io.File;
+
 public class Demo01 {
+    public static void main(String[] args) {
+        //创建一个File
+        File file = new File("C:\\Users\\比乐\\Desktop\\QQ picture");
+
+
+        boolean b = haveAVI(file);
+        System.out.println(b);
+
+    }
+
+//通过listfile.getname.endsWith判断是否存在.avi结尾文件的方法
+    public static boolean haveAVI(File file) {
+        for (File listFile : file.listFiles()) {
+            if (listFile.isFile() && listFile.getName().endsWith(".avi")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
